49b10a6928c910a5a3b16a713bf0bff8a2e65eea
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-22 14:55:36 +0000

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,32 +9,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev;
-
-	current = (*list)->next;
+	listint_t *current, *prev, *next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
+	current = (*list)->next;
+
 	while (current != NULL)
 	{
+		next = current->next;
 		prev = current->prev;
 		while (prev != NULL && prev->n > current->n)
 		{
-			if (prev->prev != NULL)
-				prev->prev->next = current;
+			prev->next = current->next;
 			if (current->next != NULL)
 				current->next->prev = prev;
-			prev->next = current->next;
-			current->prev = prev->prev;
 			current->next = prev;
+			current->prev = prev->prev;
+			if (prev->prev != NULL)
+				prev->prev->next = current;
 			prev->prev = current;
-
 			if (prev == *list)
 				*list = current;
 			print_list(*list);
 			prev = current->prev;
 		}
-		current = current->next;
+		current = next;
 	}
 }
